47f86e8c06006970cf57c7e4b41c86c63d9b0e66
Zaid-Hyplap <zaid.sayed@hyplap.com>
2022-11-22 23:48:33 +0530

    fixed some things

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -11,11 +11,11 @@ const Hero = () => {
                     <h1 class="lg--text">Ravinder Kumar</h1>
                 </div>
                 <div class="dis">
-                    <h2 class="lg--text">I build things for the WWW.</h2>
+                    <h2 class="lg--text">I ruin things for the WWW.</h2>
                 </div>
                 <div class="dis">
-                    <p>I’m a Full Stack Developer specializing in Website Building(and occasionally designing).
-                        Currently i’m Working at <a href="tcs.com" class="tcs">TCS.</a></p>
+                    <p>I’m a Full Stack Ruiner specializing in Ruining Website(and occasionally find new ways to ruin).
+                        Currently i’m Working at <a href="tcs.com" class="tcs">TCS as they hire the most useless people.</a></p>
                 </div>
                 <div className='resbtn--div'>
                     <a class="resume--btn" href="resume.pdf">Download Resume!</a>
