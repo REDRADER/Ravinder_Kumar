dcdba025fa994481c1b6197b4a48a6e57ee56872
Ravinder <hello.ravinderkumar@gmail.com>
2023-04-23 19:55:14 +0530

    change

--- src/components/About.js
+++ src/components/About.js
@@ -8,12 +8,11 @@ const About = () => {
             <div className='section--content'>
                 <div className="section--text">
                 <div>
-                        <p className='section-para1'>hi! My name is Ravinder Kumar and i like to build things which goes on Web. Mostly i like build the thing but occasionally
-                            i also do graphic designing. I started Web Development because of a teacher in 9 grade who came to take a guest lecture in
-                            our school where she first showed us that how a file writing on notepad can be displayed on the browser that was my first time seeing Hello World on a webpage.
-                        </p>
-                        <p className='section-para2'>Fast forward to today, I have been building website and Mobile apps from one to another. Recently I also joined TCS and I trying out the MNC life .
-
+                        <p className='section-para1'>Hi, I'm Ravinder Kumar, a web developer and graphic designer who loves bringing digital creations to life on the web. 
+                        My journey into web development started back in 9th grade when a guest lecturer visited our school and showed us how to display text on a web page 
+                        using a simple text editor. Seeing the "Hello World" message appear on the screen was a magical moment for me, and I knew I wanted to explore this 
+                        exciting world of web development. Fast forward to today, I have been building websites and mobile
+                         apps, one project after another, leveraging my skills in web development and graphic design to create digital experiences that engage and delight users.
                         </p>
                         <p className='section-para3'>
                             Few of the technologies i have worked with:
@@ -21,11 +20,13 @@ const About = () => {
                     </div>
                     <ul class="skills">
                         <li>JAVA</li>
+                        <li>Spring Boot</li>
                         <li>JavaScript</li>
-                        <li>Html</li>
-                        <li>Android</li>
                         <li>React</li>
-                        <li>SQL</li>
+                        <li>Html</li>
+                        
+                        <li>Python</li>
+                        <li>SQL/MySQL</li>
                         <li>Css/BootStrap</li>
                     </ul>
                 </div>
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -14,8 +14,7 @@ const Hero = () => {
                     <h2 class="lg--text">I build things for the WWW.</h2>
                 </div>
                 <div class="dis">
-                    <p>I’m a Full in Website Building(and occasionally designing).
-                        Currently i’m Working at <a href="tcs.com" class="tcs">TCS.</a></p>
+                    <p>Turning ideas into digital realities </p>
                 </div>
                 <div className='resbtn--div'>
                     <a class="resume--btn" href="resume.pdf">Download Resume!</a>
